5bf6f76708c5fef610d3fdc30eadea56a69a4511
Bendy SERVILUS <bendy.servilus.ueh.edu.ht>
2024-06-12 15:41:56 -0400

    Button close menu added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,14 @@
 function toggleMenu() {
     var navLinks = document.getElementById('nav-links');
+    var menuButtonIcon = document.querySelector('.menu-button i');
+
     if (navLinks.classList.contains('show')) {
         navLinks.classList.remove('show');
+        menuButtonIcon.classList.remove('fa-times');
+        menuButtonIcon.classList.add('fa-bars');
     } else {
         navLinks.classList.add('show');
+        menuButtonIcon.classList.remove('fa-bars');
+        menuButtonIcon.classList.add('fa-times');
     }
 }
